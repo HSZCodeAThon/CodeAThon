d3f68b043fb973e4677db88c3869f81a5e247ad7
yang song <ysong.sc@gmail.com>
2014-02-22 08:21:12 -0500

    update sql for wholeitemsset

--- wholeitemsset.sql
+++ wholeitemsset.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 22, 2014 at 09:30 AM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
+-- Generation Time: Feb 22, 2014 at 08:20 AM
+-- Server version: 5.5.35
+-- PHP Version: 5.3.10-1ubuntu3.9
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `bestcartdb`
+-- Database: `bestcart`
 --
 
 -- --------------------------------------------------------
@@ -41,12 +41,12 @@ CREATE TABLE IF NOT EXISTS `wholeitemsset` (
 INSERT INTO `wholeitemsset` (`id`, `name`, `image`, `category`) VALUES
 (10, 'apple', 'themes/assets/images/apple.jpg', 'fruit'),
 (19, 'beef', 'themes/assets/images/beef.jpg', 'meat'),
-(14, 'carrot', 'themes/assets/images/carrot.jog', 'fruit'),
 (2, 'catfish', 'themes/assets/images/catfish.jpg', 'seafood'),
 (15, 'celery', 'themes/assets/images/celery.jpg', 'fruit'),
 (20, 'chicken', 'themes/assets/images/chicken.jpg', 'meat'),
 (13, 'coriander', 'themes/assets/images/courinder.jpg', 'fruit'),
 (11, 'grape', 'themes/assets/images/grape.jpg', 'fruit'),
+(14, 'lettuce', 'themes/assets/images/lettuce.jpg', 'fruit'),
 (9, 'lobster', 'themes/assets/images/lobster.jpg', 'seafood'),
 (5, 'mussel', 'themes/assets/images/mussel.jpg', 'seafood'),
 (16, 'onion', 'themes/assets/images/onion.jpg', 'fruit'),
